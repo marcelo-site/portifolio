1c033d00abf8c065e559117648c1cd0ad3a7a7bd
Marcelo <57197501+marcelo-site@users.noreply.github.com>
2023-09-04 23:50:24 -0300

    add section certificados

---
+++ src/js/index.js
@@ -0,0 +1,67 @@
+const btnMenu = document.querySelector('.btn-abrir-menu')
+const menuMobile = document.querySelector('#menu-mobile')
+const overlay = document.querySelector('.overlay-menu')
+
+btnMenu.addEventListener('click', () => {
+    menuMobile.classList.add('abrir-menu')
+})
+
+menuMobile.addEventListener('click', () => {
+    menuMobile.classList.remove('abrir-menu')
+})
+overlay.addEventListener('click', () => {
+    menuMobile.classList.remove('abrir-menu')
+})
+
+const modal = document.querySelector('.modal')
+const modalContent = document.querySelector('.modal > div')
+const modalImg = document.querySelector('.modal img')
+const certs = document.querySelectorAll('.certs img')
+const right = document.querySelector('#right')
+const left = document.querySelector('#left')
+const exitModal = document.querySelector('#exit-modal')
+
+exitModal.addEventListener('click', () => {
+    modal.style.display = 'none'
+})
+
+const changeImg = (param) => {
+    const img = certs[param].getAttribute('src')
+    modalImg.setAttribute('src', img)
+    modal.style.display = ''
+    if(indexImg == certs.length - 1) {
+        right.style.backgroundColor = '#626262'
+    }
+    else {
+        right.style.backgroundColor = ''
+    }
+    if (indexImg === 0) {
+        left.style.backgroundColor = '#626262'
+    } else {
+        left.style.backgroundColor = ''
+    }
+    const height = modalImg.clientHeight
+    modalContent.style.height = `${height}px`
+}
+
+let indexImg = 0
+certs.forEach((cert, i) => cert.addEventListener('click', () => {
+    indexImg = i
+    changeImg(i)
+}))
+
+right.addEventListener('click', () => {
+    if (indexImg >= 0 && indexImg < certs.length - 1) {
+        indexImg++
+        changeImg(indexImg)
+    }
+})
+
+left.addEventListener('click', () => {
+    if (indexImg >= 1 && indexImg < certs.length) {
+        indexImg--
+        changeImg(indexImg)
+    }
+})
+
+

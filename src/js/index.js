3c28529e342ab370321dd576cefe4817af400c2d
Marcelo <57197501+marcelo-site@users.noreply.github.com>
2023-09-06 21:06:35 -0300

    add memo dark

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,9 @@ const btnMenu = document.querySelector('.btn-abrir-menu')
 const menuMobile = document.querySelector('#menu-mobile')
 const overlay = document.querySelector('.overlay-menu')
 
-btnMenu.addEventListener('click', () => {
-    menuMobile.classList.add('abrir-menu')
-})
-
-menuMobile.addEventListener('click', () => {
-    menuMobile.classList.remove('abrir-menu')
-})
-overlay.addEventListener('click', () => {
-    menuMobile.classList.remove('abrir-menu')
-})
+btnMenu.addEventListener('click', () => menuMobile.classList.add('abrir-menu'))
+menuMobile.addEventListener('click', () => menuMobile.classList.remove('abrir-menu'))
+overlay.addEventListener('click', () => menuMobile.classList.remove('abrir-menu'))
 
 const modal = document.querySelector('.modal')
 const modalContent = document.querySelector('.modal > div')
@@ -20,19 +13,19 @@ const certs = document.querySelectorAll('.certs img')
 const right = document.querySelector('#right')
 const left = document.querySelector('#left')
 const exitModal = document.querySelector('#exit-modal')
+const spanPlus = document.querySelector('.img-lenght span')
 
-exitModal.addEventListener('click', () => {
-    modal.style.display = 'none'
-})
+exitModal.addEventListener('click', () => modal.style.display = 'none')
 
 const changeImg = (param) => {
     const img = certs[param].getAttribute('src')
     modalImg.setAttribute('src', img)
     modal.style.display = ''
-    if(indexImg == certs.length - 1) {
+    const height = modalImg.clientHeight
+    modalContent.style.height = `${height}px`
+    if (indexImg == certs.length - 1) {
         right.style.backgroundColor = '#626262'
-    }
-    else {
+    } else {
         right.style.backgroundColor = ''
     }
     if (indexImg === 0) {
@@ -40,8 +33,6 @@ const changeImg = (param) => {
     } else {
         left.style.backgroundColor = ''
     }
-    const height = modalImg.clientHeight
-    modalContent.style.height = `${height}px`
 }
 
 let indexImg = 0
@@ -49,6 +40,7 @@ certs.forEach((cert, i) => cert.addEventListener('click', () => {
     indexImg = i
     changeImg(i)
 }))
+spanPlus.addEventListener('click', () => certs[5].click())
 
 right.addEventListener('click', () => {
     if (indexImg >= 0 && indexImg < certs.length - 1) {
@@ -56,7 +48,6 @@ right.addEventListener('click', () => {
         changeImg(indexImg)
     }
 })
-
 left.addEventListener('click', () => {
     if (indexImg >= 1 && indexImg < certs.length) {
         indexImg--
@@ -64,14 +55,26 @@ left.addEventListener('click', () => {
     }
 })
 
-const chk = document.querySelector('#chk')
+const checkDark = document.querySelector('#change-theme')
 
-chk.addEventListener('change', () => {
-   const html = document.querySelector('html')
-   html.classList.toggle('dark')
+checkDark.addEventListener('change', (check) => {
+    const html = document.querySelector('html')
     localStorage.removeItem('dark')
-
-    if(html.classList.contains('dark')) {
+    if (check.target.checked) {
+        html.classList.add('dark')
+    } else {
+        html.classList.remove('dark')
+    }
+    if (html.classList.contains('dark')) {
         localStorage.setItem('dark', 1)
     }
 })
+
+const dark_ok = () => {
+    const dark = localStorage.getItem('dark')
+    if (dark) {
+        checkDark.checked = true
+        document.querySelector('html').classList.add('dark')
+    }
+}
+dark_ok()
